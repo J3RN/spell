2b62760155bf1439b340cbfaa026c55c95970dab
Jonathan Arnett <JArnett@covermymeds.com>
2015-07-10 10:27:01 -0400

    Working solution

--- bot.rb
+++ bot.rb
@@ -1,15 +1,33 @@
 require "cinch"
-require "spell"
+require_relative "spell"
 
-Cinch::Bot.new do
+bot = Cinch::Bot.new do
   configure do |c|
+    c.nick = "spelltesttest"
     c.server = "irc.freenode.net"
     c.channels = ["#bottest"]
   end
 
-  on :message, /^spell:/ do |m|
-    m.message.split(/\s/).each do |given_word|
+  on :message, /^spell: (.+)/ do |m, sentence|
+    spell = Spell.new
 
+    correction_count =  0
+    sentence.split(/\s/).each do |given_word|
+      word = given_word.match(/[\p{L}']+/).to_s.downcase
+
+      debug "Word: #{word}"
+      debug "Spelled good? #{spell.spelled_good? word}"
+
+      if !spell.spelled_good? word
+        m.reply "#{word} is spelled wrong. Did you mean #{spell.best_match(word)}"
+        correction_count += 1
+      end
+    end
+
+    if correction_count == 0
+      m.reply "Looks good to me!"
     end
   end
 end
+
+bot.start
--- spell.rb
+++ spell.rb
@@ -1,40 +1,58 @@
-module Spell
-  class << self
-    def num_matching(one_bigrams, two_bigrams, acc = 0)
-      if one_bigrams.length == 0 || two_bigrams.length == 0
-        return acc
-      end
+require "pry"
 
-      one_two = one_bigrams.index(two_bigrams.first)
-      two_one = two_bigrams.index(one_bigrams.first)
+class Spell
+  def initialize
+    @words = File.read("words").split("\n")
+  end
 
-      if (one_two.nil? && two_one.nil?)
-        num_matching(one_bigrams.drop(1), two_bigrams.drop(1), acc)
+  def num_matching(one_bigrams, two_bigrams, acc = 0)
+    if one_bigrams.length == 0 || two_bigrams.length == 0
+      return acc
+    end
+
+    one_two = one_bigrams.index(two_bigrams.first)
+    two_one = two_bigrams.index(one_bigrams.first)
+
+    if (one_two.nil? && two_one.nil?)
+      num_matching(one_bigrams.drop(1), two_bigrams.drop(1), acc)
+    else
+      if (one_two != nil && (two_one.nil? ? true : one_two <= two_one))
+        num_matching(one_bigrams.drop(one_two + 1), two_bigrams.drop(1), acc + 1)
+      elsif (two_one != nil && (one_two.nil? ? true : two_one < one_two))
+        num_matching(one_bigrams.drop(1), two_bigrams.drop(two_one + 1), acc + 1)
       else
-        if (one_two != nil && (two_one.nil? ? true : one_two <= two_one))
-          num_matching(one_bigrams.drop(one_two + 1), two_bigrams.drop(1), acc + 1)
-        elsif (two_one != nil && (one_two.nil? ? true : two_one < one_two))
-          num_matching(one_bigrams.drop(1), two_bigrams.drop(two_one + 1), acc + 1)
-        else
-          raise 'Impossible Error'
-        end
+        raise 'Impossible Error'
       end
     end
+  end
 
-    def bigramate(word)
-      bigrams = []
-      for i in (0..(word.length - 2))
-        bigrams.push(word.slice(i, 2))
-      end
-      bigrams
+  def bigramate(word)
+    bigrams = []
+    for i in (0..(word.length - 2))
+      bigrams.push(word.slice(i, 2))
+    end
+    bigrams
+  end
+
+  def compare(word1, word2)
+    word1_bigrams = bigramate(word1)
+    word2_bigrams = bigramate(word2)
+
+    most_bigrams = [word1_bigrams.count, word2_bigrams.count].sort.last
+    num_matching(word1_bigrams, word2_bigrams).to_f / most_bigrams
+  end
+
+  def best_match(word)
+    word_hash = Hash.new {|hash, key| hash[key] = compare(word, key)}
+
+    @words.each do |word|
+      word_hash[word]
     end
 
-    def compare(word1, word2)
-      word1_bigrams = bigramate(word1)
-      word2_bigrams = bigramate(word2)
+    word_hash.sort_by {|key, value| value}.last.first
+  end
 
-      most_bigrams = [word1_bigrams.count, word2_bigrams.count].sort.last
-      num_matching(word1_bigrams, word2_bigrams).to_f / most_bigrams
-    end
+  def spelled_good?(word)
+    @words.include?(word)
   end
 end
