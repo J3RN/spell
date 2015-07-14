bfa931f0490d887be23acbd36391ffb98f33a939
Jonathan Arnett <jonarnett90@gmail.com>
2015-07-13 22:01:31 -0400

    !!join !!top\d and !!part

--- lib/bot.rb
+++ lib/bot.rb
@@ -9,6 +9,8 @@ $bot = Cinch::Bot.new do
   configure do |c|
     config = JSON.parse(File.read("settings.json"))
 
+    $master = config["master"]
+
     c.nick =      config["nick"]
     c.password =  config["password"]
     c.server =    config["server"]
@@ -31,10 +33,39 @@ $bot = Cinch::Bot.new do
     end
   end
 
-  on :message, /^top(\d)/ do |m, num_top|
+  on :message, /^!!top(\d)/ do |m, num_top|
     words_counts = $word_list.sort_by {|word, count| count }.last(num_top.to_i)
     words_counts.reverse!
     m.reply(words_counts.map {|set| "#{set.first}: #{set.last}"}.join(", "))
+  end
+
+  on :message, /^!!join (.*)/ do |m, channel_name|
+    channel_name = channel_name.strip
+
+    debug m.user.nick
+    if m.user.nick == $master
+      channel = m.bot.join(channel_name)
+
+      if channel.nil?
+        m.reply "Could not join channel #{channel_name}"
+      else
+        m.reply "Joined #{channel_name}!"
+      end
+    else
+      m.reply "Why don't you join it, eh?"
+    end
+  end
+
+  on :message, /^!!part/ do |m|
+    if m.user.nick == $master
+      if m.channel?
+        m.channel.part("Goodbye, friends")
+      else
+        m.reply "How do you expect me to do that?"
+      end
+    else
+      m.reply "How about *you* go take a hike, eh?"
+    end
   end
 
   on :message, /(.*)/ do |m, sentence|
